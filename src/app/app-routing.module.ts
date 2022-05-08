dc285899b9e81fbba74c9b06ec6c67edf22aef1a
iamchiraag <68124422+iamchiraag@users.noreply.github.com>
2022-05-08 16:05:26 +0530

    Add files via upload

---
+++ src/app/app-routing.module.ts
@@ -0,0 +1,15 @@
+import { NgModule } from '@angular/core';
+import { RouterModule, Routes } from '@angular/router';
+import { RegistrationComponent } from './registration/registration.component';
+
+const routes: Routes = [
+  { path: '', component: RegistrationComponent , },
+
+  // otherwise redirect to home
+  { path: '**', redirectTo: '' }
+];
+@NgModule({
+  imports: [RouterModule.forRoot(routes)],
+  exports: [RouterModule]
+})
+export class AppRoutingModule { }
